f21ceee448b8fca48b6941df2eb4e2e2945bfb8b
pablo koll <pablo.koll@webmefy.com>
2024-07-24 10:42:19 +0200

    implement signing method of shopify webhooks

--- src/controllers/shopify.controller.ts
+++ src/controllers/shopify.controller.ts
@@ -2,24 +2,21 @@ import crypto from 'crypto';
 import { Request, Response } from 'express';
 
 class ShopifyController {
-    SHOPIFY_ACCESS_TOKEN = process.env.SHOPIFY_ACCESS_TOKEN as string;
-
-    handleNewOrder(req: Request, res: Response) {
+    handleNewOrder(req: Request, res: Response): void {
         try {
             const hmac = req.get('X-Shopify-Hmac-Sha256') as string;
             const generatedHash = crypto
-                .createHmac('sha256', this.SHOPIFY_ACCESS_TOKEN)
+                .createHmac('sha256', `${process.env.SHOPIFY_SIGNING_SECRET}`)
                 .update(req.rawBody, 'utf8')
                 .digest('base64');
 
             if (generatedHash !== hmac) {
-                return res.status(401).send('Unauthorized');
+                res.status(401).send('Unauthorized');
             }
 
             const order = req.body;
-            console.log('Nueva orden recibida:', order);
         } catch (e) {
-            console.log(e);
+            console.log('Error in handle new order ', e);
             res.status(400).send(JSON.stringify(e));
         }
         res.status(200).send('OK');
